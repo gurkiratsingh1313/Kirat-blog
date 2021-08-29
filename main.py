ccc6cde0a74622978c1a3ef719afc9ac5bf9ed7b
Kirat <kiratriar1313@gmail.com>
2021-08-29 20:30:22 +0530

    main commit

--- main.py
+++ main.py
@@ -245,5 +245,4 @@ def delete_comment(post_id, comment_id):
     return redirect(url_for('show_post', post_id=post_id))
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(host='0.0.0.0', port=5000)
