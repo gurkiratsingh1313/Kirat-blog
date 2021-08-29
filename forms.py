66f960954f0ccfa4124dbf7434f4802584ad0f70
Kirat <kiratriar1313@gmail.com>
2021-08-29 18:51:38 +0530

    Initial Commit

---
+++ forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL, Length, Email, email_validator
+from flask_ckeditor import CKEditorField
+
+length_password = Length(min=8, message="Field must be at least 8 characters long.")
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    secret = StringField('Secret Token (Hint: Ask the developer about Token)', validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+class RegisterForm(FlaskForm):
+    name = StringField(label="Name", validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), length_password])
+    submit = SubmitField(label="Sign Me Up!")
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), length_password])
+    submit = SubmitField(label="Let Me In!")
+
+class CommentForm(FlaskForm):
+    body = CKEditorField("Comment", validators=[DataRequired()])
+    submit = SubmitField(label="ADD COMMENT")
