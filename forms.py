7cf5dc561440fb62c23b76b53f19027e2ebb2630
Kirat <kiratriar1313@gmail.com>
2021-08-29 22:27:22 +0530

    forms commit

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL, Length
+from wtforms.validators import DataRequired, URL, Length, Email
 from flask_ckeditor import CKEditorField
 
 length_password = Length(min=8, message="Field must be at least 8 characters long.")
@@ -16,15 +16,15 @@ class CreatePostForm(FlaskForm):
 
 class RegisterForm(FlaskForm):
     name = StringField(label="Name", validators=[DataRequired()])
-    email = StringField(label='Email', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
     password = PasswordField(label='Password', validators=[DataRequired(), length_password])
     submit = SubmitField(label="Sign Me Up!")
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
     password = PasswordField(label='Password', validators=[DataRequired(), length_password])
     submit = SubmitField(label="Let Me In!")
 
 class CommentForm(FlaskForm):
-    body = CKEditorField("Comment", validators=[DataRequired()])
+    body = CKEditorField("Comment", validators=[DataRequired(), Email()])
     submit = SubmitField(label="ADD COMMENT")
